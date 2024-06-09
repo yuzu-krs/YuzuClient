48070ad4fee51cc7915df907d45635abb4fa8fac
yuzu-krs <yztimai@icloud.com>
2024-06-10 02:41:18 +0900

    ESPの線を太く、プレイヤーESPの時は自分には表示しないようにした

--- src/minecraft/me/gamrboy4life/paradox/module/render/ESP.java
+++ src/minecraft/me/gamrboy4life/paradox/module/render/ESP.java
@@ -12,50 +12,50 @@ import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntityChest;
 
-public class ESP extends Module{
+public class ESP extends Module {
 
-	public ESP() {
-		super("ESP",0,Category.RENDER);
-	}
-	
-	public void setup() {
-		Paradox.instance.settingsManager.rSetting(new Setting("Player",this,false));
-		Paradox.instance.settingsManager.rSetting(new Setting("Chest",this,false));
-		Paradox.instance.settingsManager.rSetting(new Setting("HostileMobs",this,false));
-		Paradox.instance.settingsManager.rSetting(new Setting("FriendlyMobs",this,false));
-	}
-	
-	public void onRender() {
-		if(this.isToggled()&&Paradox.instance.settingsManager.getSettingByName("Player").getValBoolean()) {
-			for(Object e:mc.theWorld.loadedEntityList) {
-				if (e instanceof EntityPlayer) {
-					MobESPUtils.entityESPBox((Entity)e,0);
-				}
-			}
-		}
-		
-		if(this.isToggled()&&Paradox.instance.settingsManager.getSettingByName("Chest").getValBoolean()) {
-			for(Object o:mc.theWorld.loadedTileEntityList) {
-				if (o instanceof TileEntityChest) {
-					ChestESPUtils.blockESPBox(((TileEntityChest)o).getPos());
-				}
-			}
-		}
-		
-		if(this.isToggled()&&Paradox.instance.settingsManager.getSettingByName("HostileMobs").getValBoolean()) {
-			for(Object h:mc.theWorld.loadedEntityList) {
-				if (h instanceof EntityMob) {
-					MobESPUtils.entityESPBox((Entity)h,0);
-				}
-			}
-		}
-		
-		if(this.isToggled()&&Paradox.instance.settingsManager.getSettingByName("FriendlyMobs").getValBoolean()) {
-			for(Object f:mc.theWorld.loadedEntityList) {
-				if (f instanceof EntityAnimal) {
-					MobESPUtils.entityESPBox((Entity)f,0);
-				}
-			}
-		}
-	}
+    public ESP() {
+        super("ESP", 0, Category.RENDER);
+    }
+
+    public void setup() {
+        Paradox.instance.settingsManager.rSetting(new Setting("Player", this, false));
+        Paradox.instance.settingsManager.rSetting(new Setting("Chest", this, false));
+        Paradox.instance.settingsManager.rSetting(new Setting("HostileMobs", this, false));
+        Paradox.instance.settingsManager.rSetting(new Setting("FriendlyMobs", this, false));
+    }
+
+    public void onRender() {
+        if (this.isToggled() && Paradox.instance.settingsManager.getSettingByName("Player").getValBoolean()) {
+            for (Object e : mc.theWorld.loadedEntityList) {
+                if (e instanceof EntityPlayer && e != mc.thePlayer) {
+                    MobESPUtils.entityESPBox((Entity) e, 0);
+                }
+            }
+        }
+
+        if (this.isToggled() && Paradox.instance.settingsManager.getSettingByName("Chest").getValBoolean()) {
+            for (Object o : mc.theWorld.loadedTileEntityList) {
+                if (o instanceof TileEntityChest) {
+                    ChestESPUtils.blockESPBox(((TileEntityChest) o).getPos());
+                }
+            }
+        }
+
+        if (this.isToggled() && Paradox.instance.settingsManager.getSettingByName("HostileMobs").getValBoolean()) {
+            for (Object h : mc.theWorld.loadedEntityList) {
+                if (h instanceof EntityMob) {
+                    MobESPUtils.entityESPBox((Entity) h, 0);
+                }
+            }
+        }
+
+        if (this.isToggled() && Paradox.instance.settingsManager.getSettingByName("FriendlyMobs").getValBoolean()) {
+            for (Object f : mc.theWorld.loadedEntityList) {
+                if (f instanceof EntityAnimal) {
+                    MobESPUtils.entityESPBox((Entity) f, 0);
+                }
+            }
+        }
+    }
 }
