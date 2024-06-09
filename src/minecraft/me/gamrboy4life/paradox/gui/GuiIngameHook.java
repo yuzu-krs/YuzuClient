0e56982f73e75eb62a95270235af3fac5b68d300
yuzu-krs <yztimai@icloud.com>
2024-06-10 01:52:27 +0900

    現在使用しているModの位置と見やすく修正した

--- src/minecraft/me/gamrboy4life/paradox/gui/GuiIngameHook.java
+++ src/minecraft/me/gamrboy4life/paradox/gui/GuiIngameHook.java
@@ -5,7 +5,9 @@ import me.gamrboy4life.paradox.module.Module;
 import me.gamrboy4life.paradox.utils.ColorUtils;
 import me.gamrboy4life.paradox.utils.Wrapper;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiIngame;
+import net.minecraft.client.gui.ScaledResolution;
 
 public class GuiIngameHook extends GuiIngame{
 
@@ -27,21 +29,24 @@ public class GuiIngameHook extends GuiIngame{
 
 	
 	private void renderArrayList() {
-		int yCount=10;
-		int index=0;
-		long x=0;
+		int yCount = 0;
+		int index = 0;
+		long x = 0;
 		for(Module m : Paradox.instance.moduleManager.getModules()) {
 			m.onRender();
 			
+			ScaledResolution sr = new ScaledResolution(mc);
+			double offset = yCount*(Wrapper.fr.FONT_HEIGHT + 6);
+			
 			if(m.isToggled()) {
-				
-				Wrapper.fr.drawString("- " + m.getName(), 400, yCount -7, ColorUtils.rainbowEffect(index+x*2000000000L, 1.0F).getRGB()); // 赤色
-				yCount+=10;
-				index++;
-				x++;
+				Gui.drawRect(sr.getScaledWidth() - Wrapper.fr.getStringWidth(m.getName()) - 15, offset, sr.getScaledWidth(), 6 + Wrapper.fr.FONT_HEIGHT + offset, 0x80000000);
+	            Wrapper.fr.drawString("- " + m.getName(), sr.getScaledWidth() - Wrapper.fr.getStringWidth(m.getName()) - 13 , 4 + offset , ColorUtils.rainbowEffect(index + x*200000000L, 1.0f).getRGB());
+					yCount ++;
+					index++;
+					x++;
+
 			}
 		}
-		
 	}
 	
 }
