07911ee75c929a52cfc17d4c312ca22ca95054da
yuzu-krs <yztimai@icloud.com>
2024-06-12 17:18:49 +0900

    FPSの追加

--- src/minecraft/me/gamrboy4life/paradox/mods/ModInstances.java
+++ src/minecraft/me/gamrboy4life/paradox/mods/ModInstances.java
@@ -2,12 +2,15 @@ package me.gamrboy4life.paradox.mods;
 
 import me.gamrboy4life.paradox.gui.hud.HUDManager;
 import me.gamrboy4life.paradox.mods.impl.ModArmorStatus;
+import me.gamrboy4life.paradox.mods.impl.ModFPS;
 import me.gamrboy4life.paradox.mods.impl.ModHelloWorld;
 
 public class ModInstances {
     private static ModHelloWorld modHelloWorld;
     
     private static ModArmorStatus modArmorStatus;
+    
+    private static ModFPS modFPS;
 
     public static void register(HUDManager api) {
         modHelloWorld = new ModHelloWorld();
@@ -15,6 +18,9 @@ public class ModInstances {
         
         modArmorStatus=new ModArmorStatus();
         api.register(modArmorStatus);
+        
+        modFPS=new ModFPS();
+        api.register(modFPS);
     }
 
     public static ModHelloWorld getModHelloWorld() {
