f0da065518108d2cf4d365bc57030d9c5c756ff9
yuzu-krs <yztimai@icloud.com>
2024-06-16 07:20:41 +0900

    serverからserverへ行けるようになった

--- src/minecraft/net/minecraft/client/gui/GuiIngameMenu.java
+++ src/minecraft/net/minecraft/client/gui/GuiIngameMenu.java
@@ -3,6 +3,7 @@ package net.minecraft.client.gui;
 import java.io.IOException;
 
 import me.gamrboy4life.paradox.Paradox;
+import me.gamrboy4life.paradox.gui.GuiMultiplayerIngame;
 import me.gamrboy4life.paradox.gui.MainMenu;
 import net.minecraft.client.gui.achievement.GuiAchievements;
 import net.minecraft.client.gui.achievement.GuiStats;
@@ -34,11 +35,24 @@ public class GuiIngameMenu extends GuiScreen
 
         this.buttonList.add(new GuiButton(4, this.width / 2 - 100, this.height / 4 + 24 + i, I18n.format("menu.returnToGame", new Object[0])));
         this.buttonList.add(new GuiButton(0, this.width / 2 - 100, this.height / 4 + 96 + i, 98, 20, I18n.format("menu.options", new Object[0])));
-        GuiButton guibutton;
-        this.buttonList.add(guibutton = new GuiButton(7, this.width / 2 + 2, this.height / 4 + 96 + i, 98, 20, I18n.format("menu.shareToLan", new Object[0])));
+
+        
+        
+        if (mc.isSingleplayer()) {
+            GuiButton guibutton;    
+            this.buttonList.add(guibutton = new GuiButton(7, this.width / 2 + 2, this.height / 4 + 96 + i, 98, 20, I18n.format("menu.shareToLan", new Object[0])));
+            guibutton.enabled = this.mc.isSingleplayer() && this.mc.getIntegratedServer().getPublic();
+        } else {
+            this.buttonList.add(new GuiButton(100, this.width / 2 + 2, this.height / 4 + 96 + i, 98, 20, "Server List"));
+        }
+        
+        
+        
+        
+        
         this.buttonList.add(new GuiButton(5, this.width / 2 - 100, this.height / 4 + 48 + i, 98, 20, I18n.format("gui.achievements", new Object[0])));
         this.buttonList.add(new GuiButton(6, this.width / 2 + 2, this.height / 4 + 48 + i, 98, 20, I18n.format("gui.stats", new Object[0])));
-        guibutton.enabled = this.mc.isSingleplayer() && !this.mc.getIntegratedServer().getPublic();
+      //  guibutton.enabled = this.mc.isSingleplayer() && !this.mc.getIntegratedServer().getPublic();
     }
 
     /**
@@ -94,6 +108,11 @@ public class GuiIngameMenu extends GuiScreen
 
             case 7:
                 this.mc.displayGuiScreen(new GuiShareToLan(this));
+                break;
+                
+            case 100:
+            	this.mc.displayGuiScreen(new GuiMultiplayerIngame());
+            	break;
         }
     }
 
